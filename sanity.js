f94eb3383a4808a125e4ead3e91bda2f28c68e62
Logesh S <139110018+lokis1107@users.noreply.github.com>
2023-12-04 14:55:34 +0530

    Update sanity.js

--- sanity.js
+++ sanity.js
@@ -2,8 +2,8 @@ import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
-  projectId: "ieqxr836",
-  dataset: "production",
+  projectId: "Implementing your project id",
+  dataset: "Implementing your project dataset",
   useCdn: true,
   apiVersion: "2023-05-03",
 });
